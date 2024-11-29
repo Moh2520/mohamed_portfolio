52be0061dc5e7d6341f32ef4c4305d00631d55e0
moha1113 <mohamedibrahimma12@gmail.com>
2024-11-29 06:31:36 -0600

    Finished some small things

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,50 +1,20 @@
 import React from "react";
-
 import styles from "./About.module.css";
 import { getImageUrl } from "../../utils";
 
 export const About = () => {
   return (
     <section className={styles.container} id="about">
-      <h2 className={styles.title}>About</h2>
+      <h2 className={styles.title}>About Me</h2>
       <div className={styles.content}>
-        <img
-          src={getImageUrl("about/meinturk.png")}
-          alt="Me sitting with a laptop"
-          className={styles.aboutImage}
-        />
-        <ul className={styles.aboutItems}>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
-            <div className={styles.aboutItemText}>
-              <h3>Frontend Developer</h3>
-              <p>
-                I'm a frontend developer with experience in building responsive
-                and optimized sites
-              </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
-            <div className={styles.aboutItemText}>
-              <h3>Backend Developer</h3>
-              <p>
-                I have experience developing fast and optimised back-end systems
-                and APIs
-              </p>
-            </div>
-          </li>
-          <li className={styles.aboutItem}>
-            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
-            <div className={styles.aboutItemText}>
-              <h3>UI Designer</h3>
-              <p>
-                I have designed multiple landing pages and have created design
-                systems as well
-              </p>
-            </div>
-          </li>
-        </ul>
+        <div className={styles.textSection}>
+          <p className={styles.about}>
+            Hi there! I’m a passionate software engineer with a strong interest in cybersecurity. I thrive on solving complex problems 
+            and building innovative solutions that make a difference. Outside of coding, you’ll often find me experimenting with new recipes in the kitchen 
+            or enjoying the thrill of snowboarding down snowy slopes. Balancing creativity, technical skills, and outdoor adventures keeps me motivated 
+            and inspired every day. Some of my favorite dishes include Somali Rice, and Somali Crepes also Known as Somali Pancakes. 
+          </p>
+        </div>
       </div>
     </section>
   );
